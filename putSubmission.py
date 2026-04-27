34a27de7e606980f90d76470fac8530063ac8f51
Napat <napat.rata@dome.tu.ac.th>
2026-04-27 15:48:28 +0700

    test local TA put grade for putSubmisson

--- putSubmission.py
+++ putSubmission.py
@@ -21,3 +21,49 @@ submissions = [
         "grade": None
     }
 ]
+
+MIN_CONFIDENCE = 90
+
+#FUNCTION หลัก
+def grade_submission(submission_id, grade):
+    #check TA กรอกคะแนนผิด
+    if grade < 0 or grade > 100:
+        return {"error": "Grade must be 0-100"}
+
+    for i in submissions:
+        if i["submissionId"] == submission_id:
+
+            #check confidence
+            if i["confidence"] < MIN_CONFIDENCE:
+                return {"error": "This submission is not confident enough for TA grading"}
+
+            #check status
+            #accept --> ผ่าน
+            #failed --> ไม่ผ่าน
+            if i["status"] != "accepted":
+                return {"error": "Submission is not ready for TA grading"}
+
+            #ให้คะแนนแล้ว status เปลี่ยนจาก accepted --> graded
+            i["grade"] = grade
+            i["status"] = "graded"
+            return i
+
+    return {"error": "Submission not found"}
+
+#-----test ลองกรอกคะแนนเอง----
+print("\nAvailable submissions:")
+
+for i in submissions:
+    print("-", i["submissionId"], "| image:", i["fileKey"], "| status:",i["status"]) #งานนศทั้งหมด
+
+sid = input("\nEnter submission ID: ")
+grade = int(input("Enter grade (0-100): "))
+
+result = grade_submission(sid, grade)
+
+print("\n========== RESULT ==========")
+if "error" in result:
+    print("ERROR:", result["error"])
+else:
+    for key, value in result.items():
+        print(f"{key}: {value}")
