b8a01ca4ae8a3af5b13a7224a1814ca8aac4f7b8
Punnaphot Nakmalee <punnaphot.nak@dome.tu.ac.th>
2026-04-27 15:36:33 +0700

    FIXED Include description in lab items response

--- getLab.py
+++ getLab.py
@@ -2,10 +2,8 @@ import json
 import boto3
 
 dynamodb = boto3.resource("dynamodb")
-
 LAB_TABLE = "Lab"
 table = dynamodb.Table(LAB_TABLE)
-
 
 def response(status, body):
     return {
@@ -16,22 +14,20 @@ def response(status, body):
         },
         "body": json.dumps(body)
     }
-
-
 def lambda_handler(event, context):
     try:
         labs = []
-
         db_response = table.scan()
+
 
         for item in db_response.get("Items", []):
             labs.append({
                 "labId": item.get("labId"),
-                "title": item.get("title")
+                "title": item.get("title"),
+                "description": item.get("description")
             })
 
         return response(200, labs)
-
     except Exception as e:
         print("Error:", str(e))
         return response(500, {"error": "Failed to fetch labs"})
