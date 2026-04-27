349424ab43ef4d339a55d4ef3a1db2cc0a9efe88
chinabunchorn <chinnuttanan@gmail.com>
2026-04-27 15:32:20 +0700

    applied for lambda aws service and linked to dynamo db

--- postSubmission.py
+++ postSubmission.py
@@ -1,36 +1,39 @@
 import boto3
 import json
-import uuid
-import os
 from botocore.client import Config
 
-
-BUCKET_NAME = os.environ.get("BUCKET_NAME", "lab-st-pic")
 
 s3 = boto3.client(
     "s3",
     config=Config(signature_version="s3v4")
 )
 
+
+BUCKET_NAME = "lab-st-pic"
+
+
 def lambda_handler(event, context):
     body = json.loads(event["body"])
 
+
     filename = body["fileName"]
-    file_type = body["fileType"] 
+    file_type = body["fileType"]
     lab_id = body["labId"]
+
 
     submission_id = "sub-" + context.aws_request_id[:8]
     file_key = f"labs/{lab_id}/submissions/{submission_id}/{filename}"
+
 
     upload_url = s3.generate_presigned_url(
         "put_object",
         Params={
             "Bucket": BUCKET_NAME,
-            "Key": file_key,
-            "ContentType": file_type 
+            "Key": file_key
         },
         ExpiresIn=300
     )
+
 
     return {
         "statusCode": 200,
@@ -42,23 +45,3 @@ def lambda_handler(event, context):
         })
     }
 
-
-if __name__ == "__main__":
-    class MockContext:
-        def __init__(self):
-            
-            self.aws_request_id = str(uuid.uuid4())
-
-    mock_event = {
-        "body": json.dumps({
-            "fileName": "test_assignment.pdf",
-            "fileType": "application/pdf",
-            "labId": "cs-102"
-        })
-    }
-    mock_context = MockContext()
-
-    response = lambda_handler(mock_event, mock_context)
-
-    print("\n Response:")
-    print(json.dumps(response, indent=2))
