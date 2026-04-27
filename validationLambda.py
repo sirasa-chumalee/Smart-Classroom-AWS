267248e7c4a6f2d21c5875dc268fe54cd5049658
Akaradech Meepojproa <akaradech.mee@dome.tu.ac.th>
2026-04-27 15:38:54 +0700

    Update validationLambda : Read from Amazon S3 and add Average confidence

--- validationLambda.py
+++ validationLambda.py
@@ -20,6 +20,14 @@ textract_client = boto3.client(
     aws_session_token=MY_SESSION_TOKEN
 )
 
+s3_client = boto3.client(
+    's3',
+    region_name='us-east-1',
+    aws_access_key_id=MY_ACCESS_KEY,
+    aws_secret_access_key=MY_SECRET_KEY,
+    aws_session_token=MY_SESSION_TOKEN    
+)
+
 TARGET_KEYWORDS = {"enabled", "30days"}
 
 #Clean input text
@@ -28,17 +36,22 @@ def normalize(text):
 
 #เช็ค text กับ keyword
 def find_keywords(text_items):
-    found = {}
+    found_keywords = set()
+    confidences = []
 
     for text, confidence in text_items:
         normalized = normalize(text)
 
         for kw in TARGET_KEYWORDS:
             if kw in normalized:
-                found[kw] = max(found.get(kw, 0), confidence)
+                found_keywords.add(kw)
+                confidences.append(confidence)
 
-    return found
+    avg_conf = 0
+    if confidences:
+        avg_conf = sum(confidences) / len(confidences)
 
+    return found_keywords, avg_conf
 
 def run_rekognition(image_bytes):
     response = rekognition_client.detect_text(Image={"Bytes": image_bytes})
@@ -66,28 +79,36 @@ def run_textract(image_bytes):
 
 def lambda_handler(event, context):
 
-    #ใส่รูป
-    image_filename = "image.png"
+    bucket_name = "thisone99"
+    image_key = "image (1).png"
 
-    with open(image_filename, "rb") as f:
-        image_bytes = f.read()
+    print(f"Fetching image from S3: {image_key}")
 
-    print(f"Validating image: {image_filename}")
+    response = s3_client.get_object(
+        Bucket=bucket_name,
+        Key=image_key
+    )
+
+    image_bytes = response["Body"].read()
 
     # Tier 1: Rekognition
     try:
         print("Tier 1: Rekognition")
-        found = run_rekognition(image_bytes)
+        found, avg_conf = run_rekognition(image_bytes)
 
         #ต้องเจอ Keyword ครบทุกคำถึงจะ Pass
-        if TARGET_KEYWORDS.issubset(found.keys()):
+        if TARGET_KEYWORDS.issubset(found):
             print("Pass: Rekognition found all required keywords.")
+            r_missing=[]
             return {
                 "statusCode": 200,
                 "body": json.dumps({
-                    "status": "Pass",
+                    "avg_confidence": avg_conf,
                     "engine": "Rekognition",
-                    "keywords": found
+                    "keywordDetected": list(found),
+                    "status": "accepted",
+                    "missing_words": r_missing
+
                 })
             }
 
@@ -101,26 +122,31 @@ def lambda_handler(event, context):
         print("Tier 2: Textract")
         found = run_textract(image_bytes)
 
-        if TARGET_KEYWORDS.issubset(found.keys()):
+        if TARGET_KEYWORDS.issubset(found):
             print("Pass: Textract found all required keywords.")
+            t_missing=[]
             return {
                 "statusCode": 200,
                 "body": json.dumps({
-                    "status": "Pass",
+                    "avg_confidence": avg_conf,
                     "engine": "Textract",
-                    "keywords": found
+                    "keywordDetected": list(found),
+                    "status": "accepted",
+                    "missing_words": t_missing                  
                 })
             }
 
-        missing = list(TARGET_KEYWORDS - set(found.keys()))
+        missing = list(TARGET_KEYWORDS - set(found))
         print(f"Reject: Missing keywords: {missing}")
 
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "status": "Reject",
-                "missing": missing,
-                "detected_keywords": found
+                "avg_confidence": avg_conf,
+                "engine": "None",
+                "keywordDetected":list(found),
+                "status": "failed",
+                "missing_words": missing
             })
         }
 
